b5b7824da77d6ca68773c5622d6d1202b46a726d
温良 <yulin.zhang@ideacome.com>
2023-08-18 17:57:56 +0800

    完成

--- src/container/About/index.jsx
+++ src/container/About/index.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Header from "@/components/Header";
 
-export default function About() {
+import css from "./style.module.less";
+
+const About = () => {
+
     return (
-        <div>
-            About
-        </div>
+        <>
+            <Header title="关于" />
+            <div className={ css.about }>
+                <div className={ css.content }>
+                    <p className={ css.haihai }>嗨嗨嗨</p>
+                    <p className={ css.lailai }>快来用用我的流水账本儿</p>
+                </div>
+                <div className={ css.footer }>
+                    <p className={ css.author }>Power by hairy</p>
+                    <p className={ css.github }>Github: &nbsp;<a href="https://github.com/liegeyu/tallybook-h5">https://github.com/liegeyu/tallybook-h5</a></p>
+                </div>
+                <div className={ css.square }></div>
+                <div className={ css.circle }></div>
+                <div className={ css.trangle }></div>
+            </div>
+        </>
     )
 }
+
+export default About;
--- src/container/Account/index.jsx
+++ src/container/Account/index.jsx
@@ -1,9 +1,67 @@
 import React from "react";
+import { List, Input, Button, Toast } from "zarm";
+import { createForm } from "rc-form";
+import Header from "@/components/Header";
+import { post } from "@/utils";
 
-export default function About() {
-    return (
-        <div>
-            重置密码
+import css from "./style.module.less";
+
+const Account = (props) => {
+    const { getFieldProps, getFieldError } = props.form;
+
+    const submit = () => {
+        props.form.validateFields(async (error, value) => {
+            if (!error) {
+                if  (value.newpass !== value.newpass2) {
+                    Toast.show('两次输入密码不一致');
+                    return;
+                }
+            }
+            await post('/user/modify_pass', {
+                old_pass: value.oldpass,
+                new_pass: value.newpass,
+                new_pass2: value.newpass2
+            })
+            Toast.show('修改成功');
+        })
+    }
+
+    return <>
+        <Header title="重置密码" />
+        <div className={ css.account }>
+            <div className={ css.form }>
+                <List>
+                    <List.Item title="原密码">
+                        <Input
+                            clearable
+                            type="text"
+                            placeholder="请输入原密码"
+                            {...getFieldProps('oldpass', { rules: [{ required: true }] })}
+                        />
+                    </List.Item>
+                    <List.Item title="新密码">
+                        <Input
+                            clearable
+                            type="text"
+                            placeholder="请输入新密码"
+                            {...getFieldProps('newpass', { rules: [{ required: true }] })}
+                        />
+                    </List.Item>
+                    <List.Item title="确认密码">
+                        <Input
+                            clearable
+                            type="text"
+                            placeholder="请确认新密码"
+                            {...getFieldProps('newpass2', { rules: [{ required: true }] })}
+                        />
+                    </List.Item>
+                </List>
+            </div>
+            <Button className={ css.btn } block theme="primary" onClick={ submit }>提交</Button>
         </div>
-    )
+    </>
 }
+
+const AcountForm = createForm()(Account)
+
+export default AcountForm;
